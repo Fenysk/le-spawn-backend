d758c79dbcdd859d11a06c070fcd4a2f52687b0b
Fenysk <minefenysk@gmail.com>
2025-02-21 18:30:29 +0100

    🔥 Remove swagger

--- src/config/swagger.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SwaggerService } from './swagger.service';
-import { SwaggerAuthMiddleware } from './swagger.middleware';
-
-@Module({
-  providers: [SwaggerService, SwaggerAuthMiddleware],
-  exports: [SwaggerService],
-})
-export class SwaggerModule {}
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { RolesGuard } from './common/guards/roles.guard';
 import { JwtAccessAuthGuard } from './auth/guards/jwt-access-auth.guard';
-import { SwaggerService } from './config/swagger.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,9 +15,6 @@ async function bootstrap() {
   );
 
   app.enableCors();
-
-  const swaggerService = app.get(SwaggerService);
-  swaggerService.setup(app);
 
   await app.listen(3000);
 }
